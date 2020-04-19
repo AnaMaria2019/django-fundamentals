651a7585ba035d2ae031b121c0071cb1a3cca038
Ana <ana.maria.adina98@gmail.com>
2020-04-19 15:44:58 +0300

    Created a new app, player + configured templates directory + added urls for the initial version of the home page

--- tictactoe/tictactoe/tictactoe/settings.py
+++ tictactoe/tictactoe/tictactoe/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gameplay',
+    'player',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'tictactoe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
