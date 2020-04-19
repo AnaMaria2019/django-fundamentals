a5ff08d10ca7d0283241b26d290cc2009d4b9c60
Ana <ana.maria.adina98@gmail.com>
2020-04-19 17:20:59 +0300

    Added an Invitation class in player.models

--- tictactoe/tictactoe/player/models.py
+++ tictactoe/tictactoe/player/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Invitation(models.Model):
+    from_user = models.ForeignKey(User, related_name="invitations_sent", on_delete=models.CASCADE)
+    # 'invitations_sent' will be the name of the field inside the User class which will contain all the
+    # invitations that the current user sent.
+    to_user = models.ForeignKey(User, related_name="invitations_received", verbose_name="User to invite",
+                                help_text="Please select the user you want to play a game with", on_delete=models.CASCADE)
+    message = models.CharField(max_length=300, blank=True, verbose_name="Optional Message",
+                               help_text="It's always nice to add friendly message!")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.from_user}, {self.to_user}, {self.message}"
