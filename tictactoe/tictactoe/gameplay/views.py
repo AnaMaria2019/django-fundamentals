52f4611529c2311908f11d424502d7cd6720a46e
Ana <ana.maria.adina98@gmail.com>
2020-04-19 16:42:59 +0300

    Modified the welcome page depending on the user state: if its authenticated or not

--- tictactoe/tictactoe/gameplay/views.py
+++ tictactoe/tictactoe/gameplay/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def welcome(request):
-    return render(request, 'tictactoe/welcome.html')
+    if request.user.is_authenticated:
+        return redirect('player_home')  # 'player_home' is the name of the url where we want to redirect the user.
+        # In this case the url is '' located in 'player.urls'.
+    else:
+        return render(request, 'tictactoe/welcome.html')
